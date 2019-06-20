945fd98f6d7eaf5a7b2b927e4dc1d6b0bce953ad
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-20 18:13:56 -0400

    bookQuery.js exports titles array from hardwired userinput

--- public/scripts/bookQuery.js
+++ public/scripts/bookQuery.js
@@ -1,18 +1,29 @@
 'use strict';
-
+// query param will only return results of printType - BOOK
+//! DELETE THIS HARD WIRE WHEN USING ACTUAL INPUT
 let userInput = 'harry+potter';
-let url = 'https://www.googleapis.com/books/v1/volumes?q=' + userInput + '&printType=books';
+let url = 'https://www.googleapis.com/books/v1/volumes?q=' + userInput + '&printType=books&maxResults=10';
+
+let titles = [];
+
+function checkBookTitles(booksArray) {
+  for (let bookObj of booksArray){
+    titles.push(bookObj.volumeInfo.title);
+  }
+  console.log(titles);
+}
 
 $.ajax({
   type: "GET",
   dataType: "json",
   url: url,
   success: function (data) {
-    let jsonResult = $.parseJSON(data); 
-    console.log(jsonResult)
+    checkBookTitles(data.items);
+    console.log(data);
   },
   error: function (data) {
-    let jsonResult = $.parseJSON(data);
-    console.log('error: ', jsonResult);
+    console.log('error: ', data);
   }
 });
+
+module.exports = titles;
