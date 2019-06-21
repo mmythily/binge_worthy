cd060840b62cf201a44d565819b7cfcda306ab55
mmythily <mythily.mudunuru@gmail.com>
2019-06-21 17:38:35 -0400

    edit query to export as an object with title and boolean

--- server/routes/api/omdbQuery.js
+++ server/routes/api/omdbQuery.js
@@ -3,20 +3,10 @@
 const omdb = new (require('omdbapi'))('9d1e34f3');
 
 module.exports = {
-returnName: omdb.search({
-            search: name
-        }).then(res => {
-            console.log(res[0].title);
-        }).catch(console.error)
+    returnName: omdb.search({
+                search: name
+            }).then(res => {
+                console.log(res[0].title);
+            }).catch(console.error),
+    returnValue: true
 }
-
-// const returnMovie = (title) => {
-//     omdb.search({
-//         search: title
-//     }).then(res => {
-        
-//         console.log(res[0].title);
-//     }).catch(console.error);
-// }
-
-// console.log('name:',returnMovie(name));
