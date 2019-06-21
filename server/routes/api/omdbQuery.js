a652425c04796258915948ff05be5da0a3a65ee4
mmythily <mythily.mudunuru@gmail.com>
2019-06-21 16:49:22 -0400

    made module.exports

--- server/routes/api/omdbQuery.js
+++ server/routes/api/omdbQuery.js
@@ -2,15 +2,22 @@
 
 const omdb = new (require('omdbapi'))('9d1e34f3');
 
-
-let title = "the blues brothers";
-
-returnMovie = (title) => {
-    omdb.search({
-        search: title, 
-    }).then(res => {
-        return res[0].title;
-    }).catch(console.error);
+module.exports = {
+returnName: omdb.search({
+            search: name
+        }).then(res => {
+            
+            console.log(res[0].title);
+        }).catch(console.error)
 }
 
-console.log(returnMovie(title));
+// const returnMovie = (title) => {
+//     omdb.search({
+//         search: title
+//     }).then(res => {
+        
+//         console.log(res[0].title);
+//     }).catch(console.error);
+// }
+
+// console.log('name:',returnMovie(name));
