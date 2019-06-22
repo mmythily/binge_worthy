2d626727d99cdccc0ee39c350f1f23c76f24dc4e
mmythily <mythily.mudunuru@gmail.com>
2019-06-22 19:51:22 -0400

    append item to correct category

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,38 +1,34 @@
-const checkCat = require("../../server/utils/checkCategory.js");
 
-/* Client-side logic */
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });
-// });
-
-// $(document).ready(() => {
-//   $('#formToDo').submit(ev => {
-//     ev.preventDefault();
-//     let newItem = $('#addToDo-text').val();
-//     // see newItem.js for escape fn
-//     let escapedInput = escape(newItem);
-//     $.ajax({
-//       method: 'POST',
-//       url: '/my-list',
-//       data: {
-//         userInput: escapedInput
-//       },
-//       success: () => {
-//         renderList(escapedInput);
-//         $('#formToDo').trigger('reset');
-//       }
-//     });
-//   });
-// });
-
-
-checkCat.createCategory('Starbucks', (data) => {
-  console.log('RETURN Category: ', data);
+// /* Client-side logic */
+$(() => {
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });
 });
+
+$(document).ready(() => {
+  $('#formToDo').submit(ev => {
+    ev.preventDefault();
+    let newItem = $('#addToDo-text').val();
+    // see newItem.js for escape fn
+    let escapedInput = escape(newItem);
+    $.ajax({
+      method: 'POST',
+      url: '/my-list',
+      data: {
+        userInput: escapedInput
+      },
+      success: (result) => {
+        renderList(escapedInput, result.category);
+        $('#formToDo').trigger('reset');
+      }
+    });
+  });
+});
+
+
--- public/scripts/newItem.js
+++ public/scripts/newItem.js
@@ -1,18 +1,61 @@
+
 function escape(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
-function renderList(userInput) {
+function renderList(userInput, category) {
+ 
+  
   //let $newListItem = $('<li>').text(userInput);
   let $newListItem = 
-      `<article class='card to-read-item'>
+      `<article class='card ${category}-item'>
         ${userInput}
       </article>`
 
   //TODO - remove this temp list once new item adding to category
   let $temporaryList = $('<span>').append($newListItem);
-  $('#to-read').append($temporaryList);
-  
+  $(`#${category}`).append($temporaryList);
 }
+
+
+
+// // category
+
+// function escape(str) {
+//   let div = document.createElement('div');
+//   div.appendChild(document.createTextNode(str));
+//   return div.innerHTML;
+// }
+
+// function renderList(userInput) {
+//   //let $newListItem = $('<li>').text(userInput);
+//   let $newListItem = 
+//       `<article class='card to-read-item'>
+//         ${userInput}
+//       </article>`;
+
+//   checkCat.createCategory(userInput, (data) => {
+//     console.log(data);
+  
+//     let $temporaryList = $('<span>').append($newListItem);
+//     $(`#${data}`).append($temporaryList);
+//   });
+
+//   //TODO - remove this temp list once new item adding to category
+//   // let $temporaryList = $('<span>').append($newListItem);
+//   // $('#to-read').append($temporaryList);
+  
+// }
+
+// let userInput = 'Starbucks'
+
+// const category = checkCat.createCategory(userInput, (data) => {
+//   console.log(data);
+//   return data;
+// });
+
+// console.log(userInput)
+// category
+
