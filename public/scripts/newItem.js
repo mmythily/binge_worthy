2fda1d65c2645f2c67551411b284b698adfbb56c
mmythily <mythily.mudunuru@gmail.com>
2019-06-22 23:13:07 -0400

    bug in bookHTTP query

--- public/scripts/newItem.js
+++ public/scripts/newItem.js
@@ -6,7 +6,6 @@ function escape(str) {
 }
 
 function renderList(userInput, category) {
- 
   
   //let $newListItem = $('<li>').text(userInput);
   let $newListItem = 
@@ -15,47 +14,6 @@ function renderList(userInput, category) {
       </article>`
 
   //TODO - remove this temp list once new item adding to category
-  let $temporaryList = $('<span>').append($newListItem);
-  $(`#${category}`).append($temporaryList);
+  let $item = $('<span>').append($newListItem);
+  $(`#${category}`).append($item);
 }
-
-
-
-// // category
-
-// function escape(str) {
-//   let div = document.createElement('div');
-//   div.appendChild(document.createTextNode(str));
-//   return div.innerHTML;
-// }
-
-// function renderList(userInput) {
-//   //let $newListItem = $('<li>').text(userInput);
-//   let $newListItem = 
-//       `<article class='card to-read-item'>
-//         ${userInput}
-//       </article>`;
-
-//   checkCat.createCategory(userInput, (data) => {
-//     console.log(data);
-  
-//     let $temporaryList = $('<span>').append($newListItem);
-//     $(`#${data}`).append($temporaryList);
-//   });
-
-//   //TODO - remove this temp list once new item adding to category
-//   // let $temporaryList = $('<span>').append($newListItem);
-//   // $('#to-read').append($temporaryList);
-  
-// }
-
-// let userInput = 'Starbucks'
-
-// const category = checkCat.createCategory(userInput, (data) => {
-//   console.log(data);
-//   return data;
-// });
-
-// console.log(userInput)
-// category
-
--- server/utils/test_checkCat.js
+++ server/utils/test_checkCat.js
@@ -4,5 +4,3 @@ const checkCat = require("./checkCategory");
 checkCat.createCategory('HOWARD STERN COMES AGAIN', (data) => {
   console.log('RETURN Category: ', data);
 });
-
-
