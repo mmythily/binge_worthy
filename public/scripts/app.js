7dbf18e9ae13046c0d33a2ab4c579a4bd4110ca5
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-23 12:26:03 -0400

    remove my app.js uncomments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,25 +1,6 @@
 /* Client-side logic */
 $(document).ready(() => {
 
-  // $('#registerForm').submit(ev => {
-  //   ev.preventDefault();
-  //   console.log(ev);
-
-  //   $(() => {
-  //     $.ajax({
-  //       method: "GET",
-  //       url: "/api/registerUser"
-  //     }).done(emails => {
-  //       console.log(emails);
-        
-  //         // checkIfUserExists(users, (userData) => {
-  //         //   console.log(userData);            
-  //         // });
-  //     });
-  //   });
-  // });
-
-
   $('#formToDo').submit(ev => {
     ev.preventDefault();
     let newItem = $('#addToDo-text').val();
