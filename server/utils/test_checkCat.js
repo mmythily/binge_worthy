4a049157a98aa2f8f56116dee93cb306fe04eee2
mmythily <mythily.mudunuru@gmail.com>
2019-06-22 20:14:48 -0400

    to read category works

--- server/utils/test_checkCat.js
+++ server/utils/test_checkCat.js
@@ -1,7 +1,7 @@
 const checkCat = require("./checkCategory");
 
 
-checkCat.createCategory('godfather', (data) => {
+checkCat.createCategory('HOWARD STERN COMES AGAIN', (data) => {
   console.log('RETURN Category: ', data);
 });
 
