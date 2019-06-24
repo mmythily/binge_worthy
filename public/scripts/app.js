ab68bf5616d3715505e01ed0c677457ad0459c81
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-23 22:31:40 -0400

    cleanup files, add _header

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,21 +4,13 @@
 $(document).ready(() => {
   
   $('#updateCat').change(() => {
-
-    let newCat = $("#updateCat option").val().change();
-      console.log(newCat);
+    let newCat = $("#updateCat option").val().change(); //not working
   });
 
   $("section").on('click', 'button', (event) => {
     //event.preventDefault();
-    // console.log(event.currentTarget);
-
-    //console.log($(elementItem).attr('id'));
     let elementItem =  event.currentTarget;
-    console.log(elementItem);
-    
     let itemId = $(elementItem).attr('id');
-    
     if ($(elementItem).hasClass('buttonDelete')) {
       $.ajax({
         method: 'POST',
@@ -33,33 +25,26 @@ $(document).ready(() => {
     }      
   });
 
-  //console.log($.cookie('user_id'));
-      $.ajax({
-        method: 'GET',
-        url: '/my-list/entries',
-        success: (data) => {
-          console.log(data);
-          
-          for (let item of data){
-            let todo = item.item;
-            let cat = item.category;
-            let entryId = item.id;
-              // Catch original input seeds
-              if (cat === 'Restaurant') {
-                cat = 'to-eat';
-              } else if (cat === 'Movie') {
-                cat = 'to-watch';
-              } else if (cat === 'Book') {
-                cat = 'to-read';
-              }
-              // userInput, Category, item_id
-              renderList(todo, cat, item.id);           
-            
-          }
-          
+  $.ajax({
+    method: 'GET',
+    url: '/my-list/entries',
+    success: (data) => {
+      for (let item of data){
+        let todo = item.item;
+        let cat = item.category;
+        let entryId = item.id;
+        // Catch original input seeds
+        if (cat === 'Restaurant') {
+          cat = 'to-eat';
+        } else if (cat === 'Movie') {
+          cat = 'to-watch';
+        } else if (cat === 'Book') {
+          cat = 'to-read';
         }
-      });
-
+      renderList(todo, cat, item.id);           
+      }
+    }
+  });
 
   $('#formToDo').submit(ev => {
     ev.preventDefault();
@@ -73,7 +58,6 @@ $(document).ready(() => {
         userInput: escapedInput
       },
       success: (result) => {
-        // userInput, Category, item_id
         renderList(escapedInput, result.category, result.entryID);
         $('#formToDo').trigger('reset');
       }
--- server/server.js
+++ server/server.js
@@ -14,20 +14,8 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
-// ac
 const checkPassword = require("./utils/checkPassword");
-//const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser');
-
-//app.set('trust proxy', 1) // trust first proxy cookie-session
-
-// app.use(cookieSession({
-//   name: 'session',
-//   keys: ['moviekey', 'foodkey'],
-  
-//   // Cookie Options
-//   maxAge: 24 * 60 * 60 * 1000 // 24 hours
-// }))
 
 app.use(cookieParser());
 
@@ -53,71 +41,125 @@ app.use("../styles", sass({
 }));
 app.use(express.static("public"));
 
-
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
 
 
-// Home page
-app.get("/", (req, res) => {
-  res.render("index");
+////////////////////////////////////////////////////////////////
+////                  BROWSE / READ
+///////////////////////////////////////////////////////////////
+
+// Get main page
+app.get("/my-list", (req, res) => {
+  console.log(req.params);
+  res.render("my-list");
 });
 
+// Get user specific page
+app.get("/my-list/entries", (req, res) => {
+  let user_id = req.cookies.user_id; // cookie-something?
+  knex
+    .select('id', 'item', 'category')
+    .from('lists')
+    .where('user_id', user_id)
+    .then((rows) => {
+      let items = [];
+      for (let row of rows) {
+        items.push(rows[row]);
+      }
+      res.json(rows);
+    })
+});
+
+////////////////////////////////////////////////////////////////
+////                      EDIT
+///////////////////////////////////////////////////////////////
+
+// userInput
+app.post("/my-list", (req, res) => {
+  let cookieUser_id = req.cookies.user_id; // Getting cooking with c-parser
+  let userInput = req.body.userInput;
+  checkCat.createCategory(userInput, (data) => {
+    // add new item to lists
+    knex
+      .insert({
+        user_id: cookieUser_id,
+        item: userInput,
+        category: data
+      })
+      .into('lists')
+      .then(() => {
+        knex('lists')
+          .where({
+            user_id: cookieUser_id,
+            item: userInput
+          }).select('id')
+          .then((itemID) => {
+            console.log(itemID[0].id);
+            let itemId = itemID[0].id;
+            res.json({
+              category: data,
+              entryID: itemId
+            })
+          })
+      });
+  });
+});
+
+// Change ToDo Category
+app.post("/my-list/item/update", (req, res) => {
+  let updateCat = req.body.category;
+  // knex('lists')
+  //   .where('id', itemId)
+  //   .update()
+  //   .then(() => {
+  //     res.render('my-list');
+  //   })
+});
+
+////////////////////////////////////////////////////////////////
+////                      AND
+///////////////////////////////////////////////////////////////
+
+////////////////   LOGIN ROUTES   //////////////////
 
 // Login page
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-
 // Login post - ac
 app.post("/login", (req, res) => {
   if (req.body.email === "" || req.body.password === "") {
     res.status(400).send('Please submit a valid username and password');
   } else {
-    console.log(req.body);
     let email = req.body.email;
     let inputPassword = req.body.password;
-  
-    // (email, inputPassword, callback)
     checkPassword.authenticateLogin(email, inputPassword, (userAuth) => {
-      console.log('authValue: ', userAuth.authValue);
-
       if (userAuth.authValue === true) {
-        
-        //req.session.user_id = userAuth.id;  // cookie-parser
         res.cookie('user_id', userAuth.userId);
-        //res.render("my-list"); // This not working
-
         res.render('my-list');
       } else {
         res.send('incorrect username and password');
       }
-
     })
   }
-
-  // Success: req.session.user_id = user.id
-  // Redirect
-
 });
-    
+
+////////////////   REGISTER ROUTES   //////////////////
 
 // Register page
-app.get("/register", (reg, res) => {
+app.get("/register", (req, res) => {
   res.render("register");
 })
-
 
 // Register post
 app.post("/register", (req, res) => {
   let inputEmail = req.body.email;
   let inputPassword = req.body.password;
-
   //if email or password are empty
   if (!inputEmail) {res.status(400).send('Invalid email.');}
   if (!inputPassword) {res.status(400).send('Invalid password.');}
-
   //check if email already in db, add if not:
   knex
   .select('email', 'id')
@@ -144,90 +186,20 @@ app.post("/register", (req, res) => {
   })
 });
 
-// Get main page
-app.get("/my-list", (req, res) => {
-console.log(req.params);
-  res.render("my-list");
-});
+////////////////////////////////////////////////////////////////
+////                      DELETE
+///////////////////////////////////////////////////////////////
 
-
-
-// Get user specific page
-app.get("/my-list/entries", (req, res) => {
-  console.log(req.params);
-  
-  let user_id = req.cookies.user_id; // cookie-something?
-
-
-  knex
-  .select('id', 'item', 'category')
-  .from('lists')
-  .where('user_id', user_id)
-  .then((rows) => {
-    let items = [];
-    for (let row of rows) {
-      items.push(rows[row]);
-    }
-    res.json(rows);
-  })
-});
-
-
-// userInput
-app.post("/my-list", (req, res) => {
-  let cookieUser_id = req.cookies.user_id; // Getting cooking with c-parser
-  let userInput = req.body.userInput;
-
-  checkCat.createCategory(userInput, (data) => {
-    // Knex insert request  
-    knex
-      .insert({
-        user_id: cookieUser_id,
-        item: userInput,
-        category: data
-      })
-      .into('lists')
-      .then( () => {
-        knex('lists')
-          .where({
-            user_id: cookieUser_id,
-            item: userInput
-          }).select('id')
-        .then((itemID) => {
-        console.log(itemID[0].id);
-        let itemId = itemID[0].id;
-        res.json({category: data, entryID: itemId})
-        }) 
-      });
-  });
-  
-  //res.render("my-list");
-});
-
-
+// Delete ToDo item
 app.post("/my-list/item/delete", (req, res) => {
   let itemId = req.body.entryId;
-  console.log(itemId);
-
   knex('lists')
     .where('id', itemId)
     .del()
     .then ( () => {
       res.render('my-list');
     })
-})
-
-app.post("/my-list/item/update", (req, res) => {
-  let updateCat = req.body.category;
-  console.log(updateCat);
-
-  // knex('lists')
-  //   .where('id', itemId)
-  //   .update()
-  //   .then(() => {
-  //     res.render('my-list');
-  //   })
-})
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
